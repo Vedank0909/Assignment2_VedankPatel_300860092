1bb9416db7c7e7f3e1aa58f64432600a66e62c45
Vedank0909 <vedank0909@gmail.com>
2017-03-10 09:51:08 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+/* 
+Filename:index.js
+Authorname:Vedank Patel
+websitename:https://assignmen.herokuapp.com/
+File Description:file that routes content pages 
+*/
+let express = require('express');
+let router = express.Router();
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home'
+   });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('content/about', {
+    title: 'About'
+   });
+});
+
+
+
+
+
+/* GET products page. */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', {
+    title: 'Projects'
+   });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', {
+    title: 'Services'
+   });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact'
+   });
+});
+
+module.exports = router;
